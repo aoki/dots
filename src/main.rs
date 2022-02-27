f96a4e4aa8708bdef748e5304c8fbd4e3778c759
aoki <aoki@aoki.dev>
2022-02-28 08:25:52 +0900

    add options

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,63 @@
 use clap::Parser;
-use std::{env, fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 #[derive(Parser)]
 #[clap(version, about)]
 struct Cli {
-    #[clap(short, long, parse(from_os_str), value_name = "CONFIG_DIRECTORY")]
-    path: Option<PathBuf>,
+    #[clap(
+        short,
+        long,
+        parse(from_os_str),
+        value_name = "CONFIG_DIRECTORY",
+        env = "DOTS_CONFIG_DIR",
+        default_value = "."
+    )]
+    path: PathBuf,
 
     #[clap(
         short,
         long,
         parse(from_os_str),
         value_name = "HOME_DIRECTORY",
+        env = "DOTS_HOME_DIR",
         default_value = "~"
     )]
     home: PathBuf,
-
-    #[clap(
-        short,
-        long,
-        value_name = "ENV",
-        env = "DOTS_ENV",
-        default_value = "def"
-    )]
-    en: String,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
-    println!("{:?}", cli.en);
-
-    let dot_dir_path = match cli.path {
-        Some(path) => path,
-        None => match env::var("DOTS_CONFIG_DIR") {
-            Ok(p) => PathBuf::from(p),
-            Err(_) => PathBuf::from("."),
-        },
-    };
-
-    // let home_dir_path = match cli.home {
-    //     Some(h) => h,
-    //     None => match env::var("DOTS_HOME_DIR") {
-    //         Ok(h) => PathBuf::from(h),
-    //         Err(_) => PathBuf::from("~"),
-    //     },
-    // };
+    let dot_dir_path = cli.path;
+    let home_dir_path = cli.home;
 
     println!("{:?}", dot_dir_path);
+    println!("{:?}", home_dir_path);
 
     match fs::read_dir(dot_dir_path) {
         Err(e) => println!("{:?}", e.kind()),
         Ok(paths) => {
             for path in paths {
                 match path {
-                    Ok(p) => println!(
-                        "{}",
-                        p.path().into_os_string().to_string_lossy().to_string()
-                    ),
+                    Ok(p) => {
+                        println!("{}", p.file_name().to_string_lossy().to_string());
+                        let mut name = PathBuf::new();
+                        name.push(&home_dir_path);
+                        name.push(p.file_name());
+                        println!("{:?}", &name);
+                        fs::read_link(Path::new(&name)).is_err(println!("{:?}", &name););
+                    }
                     Err(_) => (),
                 }
-                // println!("{:?}", path);
             }
         }
     }
+
+    let test = Path::new("./foo.link");
+    let x = fs::read_link(test)?;
+    println!("{:?}", x);
+    Ok(())
 }
